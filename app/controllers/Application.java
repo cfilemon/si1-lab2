ecb595525d3b38f010f6e9835cef504eb8129ee2
C. Filemon <clenimar.filemon@gmail.com>
2015-05-29 00:52:04 -0300

    new template - tofix: its loaded twice

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Anuncio;
 import models.Instrumento;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -8,8 +9,21 @@ import views.html.index;
 
 public class Application extends Controller {
 
+    private static Form<Anuncio> form = Form.form(Anuncio.class);
+
     public static Result index() {
         return ok(index.render("Aqui tem gente que toca " + Instrumento.Craviola.getNome()));
     }
+
+    /*public static Result novoAnuncio() {
+        Form<Anuncio> formPreenchido = form.bindFromRequest();
+
+        if (formPreenchido.hasErrors()) {
+            return badRequest();
+        }
+
+        Anuncio novo = formPreenchido.get();
+        return redirect(routes.Application.index());
+    }*/
 
 }
